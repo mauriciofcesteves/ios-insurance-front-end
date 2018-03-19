e8b8bedaf771c1b899dd9d227adb7bf1a5f7276d
Mauricio Esteves <mesteves@Mauricios-MacBook-Pro.local>
2018-03-19 11:05:23 -0400

    Removing unnecessary code

--- Seguros/Model/Customer.swift
+++ Seguros/Model/Customer.swift
@@ -8,24 +8,6 @@
 
 import UIKit
 
-//open class Customer {
-//    let id: Int      = 0
-//    var ativo: Bool = false
-//    var matricula: String = ""
-//    var nome: String = ""
-//    var email: String = ""
-//    var telefone: String = ""
-//    var cpf: String = ""
-//    var cnpj: String = ""
-//    var placa: String = ""
-//    var seguradora: String = ""
-//    var titulacao: String = ""
-//    var vigenciaInicio: String = ""
-//    var vigenciaFim: String = ""
-//    var observacao: String = ""
-//}
-
-
 struct Customer: Codable {
     let id: Int
     var ativo: Bool
